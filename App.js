4201bb5539833b0504c6d949079ae4633c6f570a
mouracamila <camilapaleo@gmail.com>
2022-05-24 18:25:57 +0200

    feat: add the welcomeScreen

--- App.js
+++ App.js
@@ -1,20 +1,7 @@
-import { StyleSheet,View, SafeAreaView, StatusBar, Platform, Dimensions } from 'react-native';
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  console.log(Dimensions.get("screen"))
   return (
-    <SafeAreaView style={styles.container}>
-     <View style={{backgroundColor: 'dodgerblue', width: "50%", height: 70,}}>
-               
-     </View>
-    </SafeAreaView>
+   <WelcomeScreen/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffff ',
-    paddingTop:Platform.OS === "android"? StatusBar.currentHeight :0,
-  },
-});
