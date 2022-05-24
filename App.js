c46771bc50d1cd58b02dfc2c0e36255ad377ae36
mouracamila <camilapaleo@gmail.com>
2022-05-24 14:57:29 +0200

    feat: added the basic structure

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,View, SafeAreaView, StatusBar, Platform, Dimensions } from 'react-native';
 
 export default function App() {
+  console.log(Dimensions.get("screen"))
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+     <View style={{backgroundColor: 'dodgerblue', width: "50%", height: 70,}}>
+               
+     </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ffff ',
+    paddingTop:Platform.OS === "android"? StatusBar.currentHeight :0,
   },
 });
